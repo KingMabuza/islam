2c41d4175bf8730fbcd5d5b673a87fafe2bac0e5
KingMabuza <amal@amallevi.com>
2022-02-09 18:19:42 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,75 @@
+<body>
+<div class="navbar-fixed">
+    <nav class="transparent">
+        <div class="nav-wrapper">
+        <a href="/" class="brand-logo center white-text">Islamic Wali</a>
+        </div>
+    </nav>
+</div>
+    
+
+    <div class="soon container">
+        <div class="coming center">
+           <h2 class="center">Coming Soon</h2>
+        
+           <p class="text-flow text-justify center">Assalamualaikum warahmatullahi wabarakatuh, thank you for visiting this website, we are currently actively developing our website. We hope through this platform to share the beautiful message of Islam, defend the rights of the Ummah and be a voice for the religion and Islamic values which in recent years have been facing major opposition, . Please engage on our social media platforms to receive regular updates. </p>
+           <div class="center">
+              <a href="https://www.instagram.com/islamicwali"><i class="fa-brands fa-instagram white-text fa-xl"></i></a> <a href="https://www.twitter.com/islamicwali"><i class="fa-brands fa-twitter white-text fa-xl"></i></a>
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+</body>
+
+
+<style>
+    body{
+        background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('../static/bg.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        min-height:100vh;
+        max-height:100%;
+        width:100vw;
+        background-color: black;
+        margin:0;
+        padding:0;
+
+
+    }
+
+    @media screen and (min-width: 993px){
+        body{
+            overflow: hidden;
+        }
+    }
+
+    a{
+        font-family: centauri;
+    }
+
+    .soon{
+        display: flex;
+align-items: center;
+justify-content: center;
+height:100vh;
+
+    }
+    
+    .coming{
+        color: antiquewhite;
+      
+    }
+    p{
+        font-size: medium;
+    }
+
+    .fa-instagram{
+        padding-right: 20px;
+    }
+
+    
+    
+</style>
